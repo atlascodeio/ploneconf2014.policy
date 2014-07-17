81cb7f15ac06c905451eaf75c171a33ebc0df5b4
hellfish2 <hellfish2@gmail.com>
2014-07-17 11:46:32 -0430

    Policy working

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+Demo for mastering plone in cantv.com.ve
+
+This product may contain traces of nuts.
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='ploneconf2014.policy',
+      version=version,
+      description="Plone policy for Ploneconf2014 demo",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='Plone policy',
+      author='Carlos Malave',
+      author_email='hellfish2@gmail.com',
+      url='https://github.com/i3coding/ploneconf2014.policy',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ploneconf2014'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+        'test':['plone.app.testing'],
+      },
+      
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
